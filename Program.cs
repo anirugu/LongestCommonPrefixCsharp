649f9bfd2d809b9bedc43f53ced872b60f8a0bd3
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-10-03 06:21:51 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LongestCommonPrefixCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var minLength = strs.Min(x => x.Length);
+            // Maximum length of common prefix will be minLength;
+            for(int i = 0; i < minLength; i++)
+            {
+
+            }
+            return string.Empty;
+        }
+    }
+}
